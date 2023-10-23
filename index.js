4a6bea023d7267d0a22eba548a3ba9c65b12f074
clementmwamba <clementmwamba23@gmail.com>
2023-10-24 01:00:03 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// const input = document.getElementById("input")
+// input.addEventListener("click", function(){
+//     alert("i was clicked")
+// })
+
+// index.js
+
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener(); // Call the function to attach the event listener
+  
